154111cb1fe778fa4ece34d285bf70528630f73b
karina-mota <karinamota88@gmail.com>
2019-10-02 11:05:53 +1300

    Completed challenge

--- classes.js
+++ classes.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
+  one()
     
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -16,10 +16,53 @@ function one () {
 
   // Next, we apply a new CSS class to it:
   one.classList.add('blue')
-}
+} 
+
 
 // CREATE FUNCTION two HERE
 
+function start () {
+  
+  two()
+    
+}
+
+function two () {
+  
+  var two = document.getElementById('two')
+
+  
+  two.classList.add('blue')
+} 
+
 // CREATE FUNCTION three HERE
 
+function start () {
+  
+  three()
+    
+}
+
+function three () {
+  
+  var three = document.getElementById('three')
+
+  
+  three.classList.add('pink')
+} 
+
 // CREATE FUNCTION makeVisible HERE
+function start () {
+  
+  visible()
+    
+}
+
+function visible () {
+  
+  var visible = document.getElementsByClassName('visible')
+
+  
+  visible.classList.add('visible')
+} 
+
